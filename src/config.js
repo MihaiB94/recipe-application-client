84fe25daa71b2cffadd3e89e691261b1d525a9b7
MihaiB94 <89844187+MihaiB94@users.noreply.github.com>
2023-03-11 00:36:08 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,16 +3,3 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
    baseURL: 'https://recipe-aplication-api.onrender.com/server/'
 });
-// Add an interceptor to include the JWT token in the Authorization header
-axiosInstance.interceptors.request.use(
-   (config) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-         config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-   },
-   (error) => {
-      return Promise.reject(error);
-   }
-);
