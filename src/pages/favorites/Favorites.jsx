c11b4c8391932b9f30e8286d1682fc8833069899
MihaiB94 <89844187+MihaiB94@users.noreply.github.com>
2023-03-20 16:15:37 +0000

    Update Favorites.jsx

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -3,14 +3,19 @@ import { axiosInstance } from '../../config';
 import { ContextAPI } from '../../contextAPI/ContextAPI';
 import Recipe from '../../components/recipe/Recipe';
 
+import jwt_decode from 'jwt-decode';
 const Favorites = () => {
    const [favorites, setFavorites] = useState([]);
    const [error, setError] = useState(null);
    const { user, dispatch } = useContext(ContextAPI);
-
+   const decodedToken = jwt_decode(user.token);
    useEffect(() => {
       axiosInstance
-         .get(`https://recipe-aplication-api.onrender.com/server/users/${user._id}/favorites`)
+         .get(`https://recipe-aplication-api.onrender.com/server/users/${user.id}/favorites`, {
+            headers: {
+               Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+         })
          .then((res) => setFavorites(res.data))
          .catch((err) => setError(err));
    }, []);
@@ -19,9 +24,9 @@ const Favorites = () => {
       <div className="recipes">
          <div className="recipes-container">
             {error && <p>{error.message}</p>}
-        {Array.isArray(favorites) && favorites.map((recipe) => (
-  <Recipe key={recipe._id} recipe={recipe} />
-))}
+            {favorites.map((recipe) => (
+               <Recipe key={recipe._id} recipe={recipe} />
+            ))}
          </div>
       </div>
    );
